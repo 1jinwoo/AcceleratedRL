2094b65b6d63ce5713d8f50445a30bb3b8435f8b
Qi-Zha0 <mz2651@columbia.edu>
2019-12-11 07:00:09 +0000

    finished policy_rollout

--- policy_rollout.py
+++ policy_rollout.py
@@ -1,84 +1,69 @@
+"""
+output a .bk2 video of a (pre-trained model) agent playing a game
+"""
+
 import retro
 import os, inspect
 import pickle
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-os.sys.path.insert(0, parentdir)
+from baselines.ppo2.model import Model
 from baselines.common.vec_env import SubprocVecEnv
 from baselines.common.retro_wrappers import make_retro, wrap_deepmind_retro
 from baselines.common.policies import build_policy
 from train_ppo import ppo2
+from train_ppo.runner import Runner
+import retro
+import matplotlib.pyplot as plt
 try:
     from mpi4py import MPI
 except ImportError:
     MPI = None
-from train_ppo.runner import Runner
-import retro
 
-game = 'Pitfall-Atari2600'
-state = retro.State.DEFAULT
-scenario = 'scenario'
-record = True
-verbose = 1
-quiet = 0
-obs_type = 'image'
-players = 1
-dir_note = '_rl_baseline1'
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+os.sys.path.insert(0, parentdir)
 
 def main():
     def make_env():
         obs_type = retro.Observations.IMAGE  # retro.Observations.RAM
-        env = retro.make(game=game, state=state, scenario=scenario, record=record, players=players, obs_type=obs_type)
-        # env = retro.make(game=game, state=state, scenario=scenario)
+        env = retro.make(game='Pitfall-Atari2600', state=retro.State.DEFAULT, scenario='scenario', record='.', 
+                         players=1, obs_type=obs_type)
         env = wrap_deepmind_retro(env)
         return env
 
     base_dirname = os.path.join(currentdir, "results")
-
-    dir_name = "pitfall_ppo2_rl_baseline1"
+    #dir_name = "pitfall_ppo2_rl_baseline1"
+    dir_name = "test"
     dir_name = os.path.join(base_dirname, dir_name)
-    load_path = os.path.join(dir_name, )
+    load_path = os.path.join(dir_name, 'models/00390')
 
-    venv = SubprocVecEnv([make_env] * 1)
+    venv = SubprocVecEnv([make_env] * 1) #Vectorized
     network = 'cnn'
     policy = build_policy(venv, network)
-    # Get the nb of env
-    nenvs = venv.num_envs
+    nenvs = venv.num_envs  # Get the nb of env
 
     # Get state_space and action_space
     ob_space = venv.observation_space
     ac_space = venv.action_space
 
-
-
-
-    # Instantiate the model object (that creates act_model and train_model)
-    from baselines.ppo2.model import Model
+    # Instantiate the model object
     model_fn = Model
-
-    model = model_fn(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act=nenvs)
+    nsteps=2048
+    nbatch = nenvs * nsteps
+    nminibatches= 4
+    nbatch_train = nbatch // nminibatches
+    model = model_fn(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act=nenvs,
+                     nbatch_train=nbatch_train, nsteps=2048, ent_coef=0.0, vf_coef=0.5,  max_grad_norm=0.5)
     model.load(load_path)
+    
     # Instantiate the runner object
-    runner = Runner(env=venv, model=model, nsteps=nsteps, gamma=gamma, lam=lam)
+    runner = Runner(env=venv, model=model, nsteps=nsteps, gamma=0.99, lam=0.95)
 
-    performance_fname = os.path.join(dir_name, "performance.p")
-    with open(performance_fname, "wb") as f:
-        pickle.dump(performance, f)
-
-    import matplotlib.pyplot as plt
-    plt.figure()
-    plt.plot(performance["policy_loss"], label="policy_loss")
-    plt.plot(performance["value_loss"], label="value_loss")
-    plt.legend()
-    plt.savefig(os.path.join(dir_name, "loss.jpg"), dpi=300)
-    plt.close()
-    plt.figure()
-    plt.plot(performance["reward"], label="reward")
-    plt.legend()
-    plt.savefig(os.path.join(dir_name, "reward.jpg"), dpi=300)
-    plt.close('all')
-
+    # run the Runner and record video
+    total_timesteps=int(1e4)
+    nupdates = total_timesteps//nbatch
+    for update in range(1, nupdates+1):
+        print("progress: ", update, "/", nupdates)
+        obs, returns, masks, actions, values, neglogpacs, states, epinfos = runner.run()
 
 if __name__ == '__main__':
     main()
